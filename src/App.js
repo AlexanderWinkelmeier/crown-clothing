525020eca44798abc84fc51d2092bafefcea2d5f
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2022-11-13 07:42:16 +0100

    Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+// import Navbar from './components/navbar/navbar.component';
 
 const Shop = () => {
   return <h1>I am the Shop Route</h1>;
 };
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <Routes>
-           {' '}
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
-         {' '}
     </Routes>
   );
 };
