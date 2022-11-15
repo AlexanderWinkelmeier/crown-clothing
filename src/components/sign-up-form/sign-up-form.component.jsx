c91a60368f47f5b111009a56eb2fa6e74388393a
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2022-11-15 11:20:22 +0100

    Sign Up Form Part 1

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target; // event.target ist das Input-Field
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          required
+          onChange={handleChange} // wenn der User etwas eingibt, wir der Event-Handler handleChange aufgerufen und ausgeführt
+          name="displayName" // sitzt auf dem event.target, d.h. input-Feld, und kann abgerufen werden
+          value={displayName} // das, was der User sieht
+        />
+
+        <label>Email</label>
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
+        <button type="submit">SIGN UP</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
