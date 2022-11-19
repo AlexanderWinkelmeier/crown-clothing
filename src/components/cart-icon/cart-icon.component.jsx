79bf0d499a80574957ca9404d96a2449bbb2ac89
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2022-11-19 06:31:42 +0100

    Toggle Cart Open Lesson-13

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,27 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+  // const toggleIsCartOpen = () => {
+  //   if (isCartOpen) {
+  //     setIsCartOpen(false);
+  //   } else {
+  //     setIsCartOpen(true);
+  //   }
+  // };
+
+  // Kurzform
+
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">1</span>
     </div>
