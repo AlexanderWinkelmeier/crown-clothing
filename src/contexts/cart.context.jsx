3d20b06a300c957e95e097bf6a1584cfd2e8ae08
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2022-11-20 09:04:13 +0100

    Cart Items Design Lesson-15

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,10 +1,11 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const addCartItem = (cartItems, productToAdd) => {
+  // befindet sich das hinzugefügte Produkt bereits im Warenkorb
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
   );
-
+  // a) es befindet sich bereits im Warenkorb --> Menge um eins erhöhen
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === productToAdd.id
@@ -12,7 +13,7 @@ export const addCartItem = (cartItems, productToAdd) => {
         : cartItem
     );
   }
-
+  // es befindet sich noch nicht im Warenkorb: Produkt dem Warenkorb hinzufügen
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
@@ -21,16 +22,37 @@ export const CartContext = createContext({
   setIsOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
 
+  // Funktion mit Logik zum Ändern des Warenkorbs
+  // hinsichtlich Inhalb bzw. Menge der Produkte
   const addItemToCart = (product) =>
     setCartItems(addCartItem(cartItems, product));
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  useEffect(() => {
+    // total ist der accumulator cartItem das currentElement
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+  // immer wenn sich irgendeine Property von cartItems ändert; hier: die quantity
+  // dann soll die Menge der cartItems neu berechnet werden
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
