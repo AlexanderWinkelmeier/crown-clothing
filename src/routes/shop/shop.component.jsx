b8fb0249b1b07cd91dbd78f2238dc3d7a300c201
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2022-11-24 09:38:29 +0100

    Category Page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
 const Shop = () => {
   return (
     <Routes>
+      {/* Haupt-Route */}
       <Route index element={<CategoriesPreview />} />
+      {/* Sub-Routes: wenn in die URL /shop/sneakers eingegeben oder darauf umgeleitet wird,
+      kann in der Komponente "Category" über den Hook useParams darauf zugegriffen werden;
+      In App.js muss über shop/* angegeben werden, dass shop Sub-Routes hat */}
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
