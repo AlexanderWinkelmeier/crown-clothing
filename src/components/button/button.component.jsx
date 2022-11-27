82e5e949d82dd90f366c3d8ebe636e2ba9829307
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2022-11-27 08:24:06 +0100

    Styled-Component - Cart-Dropdown

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -10,7 +10,7 @@ export const BUTTON_TYPE_CLASSES = {
   inverted: 'inverted',
 };
 
-// Eine Hilfs-Funktion, der man den ButtonType übergibt und dann den jeweiligen ButtonStyle zurückgibt
+// Eine Hilfs-Funktion, der man den ButtonType übergibt und dann einen der drei ButtonComponents (Button-Styles) zurückerhält
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
   ({
@@ -18,6 +18,12 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
+
+// ({declares object properties}[propertyToAccess])
+// es werden bei getButton die properties eines Objekts, das sich in den geschweiften Klammern befindet, deklariert
+// darunter in eckigen Klammern steht unter welcher Bezeichnung diese Properties im Objekt erreichbar sein sollen
+// hier unter buttonType
+// übergibt man der Funktion z.B. BUTTON_TYPE_CLASSES.google erhält man den GoogleSignInButton zurück
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
