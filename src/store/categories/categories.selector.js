9bb4ca8ce10922dd15c4a3f0e3ef575ed49ed7a7
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2022-12-12 06:55:37 +0100

    Reselect Library

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,13 +1,32 @@
-export const selectCategoriesMap = (state) => {
-  console.log('Selector fired');
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category; // destructuring von category, d.h. eines einzelnen Elements aus dem Array
-    acc[title.toLowerCase()] = items;
-    // Zugriff auf ein Element des Objekts mittels Klammer-Notation und den items zuweisen
-    // die einzelnen kleingeschriebenen title sollen die items  des JavaScript-Objekts sein
-    return acc;
-  }, {});
-};
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories; // State-Slice
+
+// ! 1. Memoized Selektor
+export const selectCategories = createSelector(
+  // Array bestehend aus Input-Selektoren:
+  // welche Slices von Redux sollen verwendet werden, um einen Output zu erzeugen
+  // hier: nur der selectCategoryReducer, d.h. state.categories
+  [selectCategoryReducer],
+  // der Output-Selektor mit den Argumenten aus den Input-Selektoren, hier: nur ein Argument
+  // und den Werten aus dieser Slice, den categories-Werten
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// Der memoized Selektor wird nur dann ausgeführt, wenn sich der Input-Selektor "selectCategoryReducer"
+// verändert hat, ansonsten nicht
+
+// ! 2. Memoized Selektor
+export const selectCategoriesMap = createSelector(
+  [selectCategories], // hier ist der Input-Selektor, der obige memoized Selektor mit den categories-Array als Output
+  // Output-Selektor
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
 
 // Hier wird ein Array (state.categories.categories) in ein JavaScript-Objekt (map) transformiert:
 // Der Startwert ist ein leeres JavaScript-Object {}; das ist der acc (Akkumulator) beim ersten Durchlauf
