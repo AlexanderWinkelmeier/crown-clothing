73e3f5e3c09bbad49a2743565707f78f63da052c
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-01-10 07:11:12 +0100

    Typing User Saga Part 1

--- src/store/user/user.saga.ts
+++ src/store/user/user.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'typed-redux-saga/macro';
 import { USER_ACTION_TYPES } from './user.types';
 import {
   signInSuccess,
@@ -7,6 +7,7 @@ import {
   signUpFailed,
   signOutSuccess,
   signOutFailed,
+  EmailSignInStart,
 } from './user.action';
 import {
   getCurrentUser,
@@ -15,38 +16,48 @@ import {
   signInAuthUserWithEmailAndPassword,
   createAuthUserWithEmailAndPassword,
   signOutUser,
+  AdditionalInformation,
 } from '../../utils/firebase/firebase.utils';
+
+import { User } from 'firebase/auth';
 
 // ? ###################   CHECK USER SESSION   #########################################
 
 // 4)
 // "Worker-Saga 2": holt Daten vom Benutzer
-export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
+export function* getSnapshotFromUserAuth(
+  userAuth: User,
+  additionalDetails?: AdditionalInformation
+) {
   try {
-    const userSnapShot = yield call(
+    const userSnapShot = yield* call(
       createUserDocumentFromAuth, // --> Firebase
       userAuth,
       additionalDetails
     );
-    yield put(signInSuccess({ id: userSnapShot.id, ...userSnapShot.data() })); // --> Reducer
+    if (userSnapShot) {
+      yield* put(
+        signInSuccess({ id: userSnapShot.id, ...userSnapShot.data() })
+      ); // --> Reducer
+    }
   } catch (error) {
-    yield put(signInFailed(error)); // --> Reducer
+    yield* put(signInFailed(error as Error)); // --> Reducer
   }
 }
 // 3) "Worker-Saga 1": prüft, ob der Benutzer angemeldet ist
 export function* isUserAuthenticated() {
   try {
-    const userAuth = yield call(getCurrentUser); // --> firebase
+    const userAuth = yield* call(getCurrentUser); // --> firebase
     if (!userAuth) return;
-    yield call(getSnapshotFromUserAuth, userAuth); // --> "Worker-Saga 2"
+    yield* call(getSnapshotFromUserAuth, userAuth); // --> "Worker-Saga 2"
   } catch (error) {
-    yield put(signInFailed(error)); // --> Reducer
+    yield* put(signInFailed(error as Error)); // --> Reducer
   }
 }
 
 // 2) "Watcher-Saga A", die auf "CHECK_USER_SESSION" horcht, die in app.js in useEffect dispatched wird
 export function* onCheckUserSession() {
-  yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated); // --> "Worker-Saga 1"
+  yield* takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated); // --> "Worker-Saga 1"
 }
 // ? ##################      END CHECK USER SESSION   ###############################################################
 
@@ -55,16 +66,16 @@ export function* onCheckUserSession() {
 // c) "Worker-Saga I"
 export function* signInWithGoogle() {
   try {
-    const { user } = yield call(signInWithGooglePopup); // --> firebase
-    yield call(getSnapshotFromUserAuth, user); // --> "Worker-Saga 2"
+    const { user } = yield* call(signInWithGooglePopup); // --> firebase
+    yield* call(getSnapshotFromUserAuth, user); // --> "Worker-Saga 2"
   } catch (error) {
-    yield put(signInFailed(error)); // --> Reducer
+    yield* put(signInFailed(error as Error)); // --> Reducer
   }
 }
 
 // b) "Watcher-Saga B", die auf "GOOGLE_SIGN_IN_START" horcht, die sich in sign-in-form-component.jsx befindet
 export function* onGoogleSignInStart() {
-  yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle); // -- "Worker-Saga I"
+  yield* takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle); // -- "Worker-Saga I"
 }
 
 // * ###############   END SIGN IN WITH GOOGLE ##############################################
@@ -73,22 +84,28 @@ export function* onGoogleSignInStart() {
 
 // z) "Worker-Saga alpha"
 
-export function* signInWithEmail({ payload: { email, password } }) {
+export function* signInWithEmail({
+  payload: { email, password },
+}: EmailSignInStart) {
   try {
-    const { user } = yield call(
+    const userCredential = yield* call(
       signInAuthUserWithEmailAndPassword,
       email,
       password
     ); // --> firebase
-    yield call(getSnapshotFromUserAuth, user); // --> "Worker-Saga 2"
+
+    if (userCredential) {
+      const { user } = userCredential;
+      yield* call(getSnapshotFromUserAuth, user); // --> "Worker-Saga 2"
+    }
   } catch (error) {
-    yield put(signInFailed(error)); // --> Reducer
+    yield* put(signInFailed(error as Error)); // --> Reducer
   }
 }
 
 // y) "Watcher-Saga X", die auf "EMAIL_SIGN_IN_START" horcht, die sich in sign-in-form-component.jsx befindet
 export function* onEmailSignInStart() {
-  yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail); // --> "Worker-Saga alpha"
+  yield* takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail); // --> "Worker-Saga alpha"
 }
 
 // ! ############## END SIGN IN WITH E-MAIL AND PASSWORD ##############################
@@ -97,30 +114,30 @@ export function* onEmailSignInStart() {
 
 // "Worker-Saga"
 export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
-  yield call(getSnapshotFromUserAuth, user, additionalDetails); // --> "Worker-Saga 2"
+  yield* call(getSnapshotFromUserAuth, user, additionalDetails); // --> "Worker-Saga 2"
 }
 
 // "Watcher-Saga"
 export function* onSignUpSuccess() {
-  yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
+  yield* takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
 }
 
 // "Worker-Saga"
 export function* signUp({ payload: { email, password, displayName } }) {
   try {
-    const { user } = yield call(
+    const { user } = yield* call(
       createAuthUserWithEmailAndPassword, // --> firebase
       email,
       password
     );
-    yield put(signUpSuccess(user, { displayName })); // --> User-Saga, nicht Reducer!
+    yield* put(signUpSuccess(user, { displayName })); // --> User-Saga, nicht Reducer!
   } catch (error) {
-    yield put(signUpFailed(error)); // --> User-Saga, nicht Reducer!
+    yield* put(signUpFailed(error)); // --> User-Saga, nicht Reducer!
   }
 }
 // "Watcher-Saga"
 export function* onSignUpStart() {
-  yield takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
+  yield* takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
 }
 // ? ############### END SIGN UP ####################################################
 
@@ -129,22 +146,22 @@ export function* onSignUpStart() {
 // "Worker-Saga"
 export function* signOut() {
   try {
-    yield call(signOutUser); // --> firebase
-    yield put(signOutSuccess()); // --> Reducer
+    yield* call(signOutUser); // --> firebase
+    yield* put(signOutSuccess()); // --> Reducer
   } catch (error) {
-    yield put(signOutFailed(error)); // --> Reducer
+    yield* put(signOutFailed(error)); // --> Reducer
   }
 }
 
 // "Watcher-Saga"
 export function* onSignOutStart() {
-  yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
+  yield* takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
 }
 // * ############### END  SIGN OUT ###################################################
 
 // 5)
 export function* userSagas() {
-  yield all([
+  yield* all([
     call(onCheckUserSession),
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
