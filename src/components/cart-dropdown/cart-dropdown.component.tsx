3b9aef903e32f4158c410cbbfdf7381500c9a2cc
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-01-16 08:00:10 +0100

    Re-Set useMemo-Hook

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,11 +1,11 @@
-import { useState, useMemo, useCallback } from 'react';
+import { useCallback } from 'react';
 
-import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-import { selectCartItems } from '../../store/cart/cart.selector';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../store/cart/cart.selector';
 
 import {
   CartDropdownContainer,
@@ -13,31 +13,9 @@ import {
   CartItems,
 } from './cart-dropdown.styles';
 
-const sleep = (milliseconds: number): void => {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if (new Date().getTime() - start > milliseconds) {
-      break;
-    }
-  }
-};
-
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
-
-  const [count, setCount] = useState(0);
-
-  // zeitintensive Funktion (2000 ms), die bei jedem Re-rendering der Komponente
-  // den Rückgabewert neu berechnet, wenn kein useMemo-Hook verwendet wird
-  // wird ein useMemo-Hook verwendet, wird der Rückgabewert neu berechnet, wenn
-  // sich ein Wert in der Dependency-Array ändert; hier: count
-  const hundredCount = useMemo(() => {
-    console.log('start');
-    sleep(2000);
-    console.log('end');
-    return 100 + count;
-  }, [count]);
 
   const goToCheckoutHandler = useCallback(() => {
     navigate('/checkout');
@@ -46,14 +24,13 @@ const CartDropdown = () => {
   return (
     <CartDropdownContainer>
       <CartItems>
-        {hundredCount}
-        {/* {cartItems.length ? (
+        {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
           <EmptyMessage>Your cart is empty</EmptyMessage>
-        )} */}
+        )}
       </CartItems>
-      <Button onClick={() => setCount(count + 1)}>CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </CartDropdownContainer>
   );
 };
