b5182a045ab1c98720342260cb896ed6eea288d1
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-01-16 07:56:32 +0100

    Hooks: useMemo

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -13,9 +13,31 @@ import {
   CartItems,
 } from './cart-dropdown.styles';
 
+const sleep = (milliseconds: number): void => {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+};
+
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
+
+  const [count, setCount] = useState(0);
+
+  // zeitintensive Funktion (2000 ms), die bei jedem Re-rendering der Komponente
+  // den Rückgabewert neu berechnet, wenn kein useMemo-Hook verwendet wird
+  // wird ein useMemo-Hook verwendet, wird der Rückgabewert neu berechnet, wenn
+  // sich ein Wert in der Dependency-Array ändert; hier: count
+  const hundredCount = useMemo(() => {
+    console.log('start');
+    sleep(2000);
+    console.log('end');
+    return 100 + count;
+  }, [count]);
 
   const goToCheckoutHandler = useCallback(() => {
     navigate('/checkout');
@@ -24,13 +46,14 @@ const CartDropdown = () => {
   return (
     <CartDropdownContainer>
       <CartItems>
-        {cartItems.length ? (
+        {hundredCount}
+        {/* {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
           <EmptyMessage>Your cart is empty</EmptyMessage>
-        )}
+        )} */}
       </CartItems>
-      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
+      <Button onClick={() => setCount(count + 1)}>CHECKOUT</Button>
     </CartDropdownContainer>
   );
 };
